a48fe9b8357fb3c5fe2731b066c65596ed9cb0be
Oleksandr Tsabak <sasha.tsabak@gmail.com>
2022-10-28 17:13:02 +0300

    part 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,37 @@
 import styled from 'styled-components';
-import { HiArchive, HiArrowCircleUp, HiBriefcase } from 'react-icons/hi';
+import { AiFillLike, AiFillMeh, AiFillDislike } from 'react-icons/ai';
 import { Button } from './Button';
 import { Box } from './Box';
 
-const Header = styled.p`
+const Header = styled.h1`
   color: ${p => p.theme.colors.accent};
   font-family: ${p => p.theme.fonts.body};
   font-size: ${p => p.theme.fontSizes.l};
-  padding-top: ${p => p.theme.space[4]}px;
-  padding-bottom: ${p => p.theme.space[3]}px;
+  padding-top: ${p => p.theme.space[2]}px;
+  padding-bottom: ${p => p.theme.space[2]}px;
+`;
+const HeaderPart = styled.h2`
+  color: ${p => p.theme.colors.accent};
+  font-family: ${p => p.theme.fonts.body};
+  font-size: ${p => p.theme.fontSizes.m};
+  padding-top: ${p => p.theme.space[2]}px;
+  padding-bottom: ${p => p.theme.space[2]}px;
 `;
 
 export const App = () => {
   return (
     <Box position="relative" as="main">
-      <Box display="flex" justifyContent="flex-end" mt={4} as="section">
+      <Box display="flex" justifyContent="center" mt={2} as="section">
         <Header>Please leave feedback</Header>
       </Box>
 
-      <Box as="section" mt={6}>
-        <Button icon={HiArchive}>Good</Button>
-        <Button icon={HiArrowCircleUp}>Netral</Button>
-        <Button icon={HiBriefcase}>Bad</Button>
+      <Box display="flex" justifyContent="center" gap="20px" as="section">
+        <Button icon={AiFillLike}>Good</Button>
+        <Button icon={AiFillMeh}>Netral</Button>
+        <Button icon={AiFillDislike}>Bad</Button>
+      </Box>
+      <Box display="flex" justifyContent="center">
+        <HeaderPart>Statistics</HeaderPart>
       </Box>
     </Box>
   );
