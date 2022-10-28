a3a244f33debaf3dbc4ef3e1fa8c2539a3831ba9
Oleksandr Tsabak <sasha.tsabak@gmail.com>
2022-10-28 16:19:02 +0300

    step 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import styled from 'styled-components';
+import { HiArchive, HiArrowCircleUp, HiBriefcase } from 'react-icons/hi';
+import { Button } from './Button';
+import { Box } from './Box';
+
+const Header = styled.p`
+  color: ${p => p.theme.colors.accent};
+  font-family: ${p => p.theme.fonts.body};
+  font-size: ${p => p.theme.fontSizes.l};
+  padding-top: ${p => p.theme.space[4]}px;
+  padding-bottom: ${p => p.theme.space[3]}px;
+`;
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Box position="relative" as="main">
+      <Box display="flex" justifyContent="flex-end" mt={4} as="section">
+        <Header>Please leave feedback</Header>
+      </Box>
+
+      <Box as="section" mt={6}>
+        <Button icon={HiArchive}>Good</Button>
+        <Button icon={HiArrowCircleUp}>Netral</Button>
+        <Button icon={HiBriefcase}>Bad</Button>
+      </Box>
+    </Box>
   );
 };
