fd37f178fffb1d4df6e614e84bb9462d74d1bbd0
Oleksandr Tsabak <sasha.tsabak@gmail.com>
2022-11-03 23:32:56 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,39 +3,83 @@ import React from 'react';
 // import { AiFillLike, AiFillMeh, AiFillDislike } from 'react-icons/ai';
 // import { Button } from './Button';
 import { Box } from './Box';
-import Feedback from './Feedback/Feedback';
+import FeedbackOptions from './Feedback/Feedback';
 import Statistics from './Statistics/Statistics';
+import Section from './Section/Section';
+import Notification from './Notification/Notification';
 
 export class App extends React.Component {
   state = {
+    visible: false,
     good: 0,
     neutral: 0,
     bad: 0,
+    total: 0,
+    positiveFeedbeck: 0,
   };
 
   addGood = () => {
-    this.setState(prevState => ({ good: prevState.good + 1 }));
+    //  console.log('positiveFeedbeck', this.state.positiveFeedbeck);
+    //  console.log('total', this.state.total);
+    this.setState({ visible: true });
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+      total: prevState.good + 1 + prevState.neutral + prevState.bad,
+      positiveFeedbeck: Math.round(
+        ((prevState.good + 1) * 100) / (prevState.total + 1)
+      ),
+    }));
   };
   addNeutral = () => {
-    this.setState(prevState => ({ neutral: prevState.neutral + 1 }));
+    //  console.log('neutral', this.state.neutral);
+    //  console.log('total', this.state.total);
+    this.setState({ visible: true });
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+      total: prevState.good + prevState.neutral + 1 + prevState.bad,
+      positiveFeedbeck: Math.round(
+        (prevState.good * 100) / (prevState.total + 1)
+      ),
+    }));
   };
   addBad = () => {
-    this.setState(prevState => ({ bad: prevState.bad + 1 }));
+    //  console.log('total', this.state.total);
+    this.setState({ visible: true });
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+      total: prevState.good + prevState.neutral + prevState.bad + 1,
+      positiveFeedbeck: Math.round(
+        (prevState.good * 100) / (prevState.total + 1)
+      ),
+    }));
   };
 
   render() {
     return (
       <Box position="relative" as="main">
-        <Feedback
+        <Section title="Please leave feedback">Please leave feedback </Section>
+        <FeedbackOptions
           onAddGood={this.addGood}
           onAddNeutral={this.addNeutral}
           onAddBad={this.addBad}
         />
-        <Statistics
-          onStateGood={this.state.good}
-          onStateNeutral={this.state.neutral}
-          onStateBad={this.state.bad}
-        />
+
+        <Section title="Statistics">Statistics </Section>
+        {!this.state.visible && (
+          <Notification message="There is no feedback">
+            There is no feedback
+          </Notification>
+        )}
+
+        {this.state.visible && (
+          <Statistics
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={this.state.total}
+            positivePercentage={this.state.positiveFeedbeck}
+          />
+        )}
       </Box>
     );
   }
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,40 +1,34 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Box } from '../Box';
 
-const HeaderPart = styled.h2`
-  color: ${p => p.theme.colors.accent};
-  font-family: ${p => p.theme.fonts.body};
-  font-size: ${p => p.theme.fontSizes.m};
-  padding-top: ${p => p.theme.space[2]}px;
-  padding-bottom: ${p => p.theme.space[2]}px;
-`;
-
-const Statistics = ({ onStateGood, onStateNeutral, onStateBad }) => (
-  <Box>
-    <Box display="flex" justifyContent="center">
-      <HeaderPart>Statistics</HeaderPart>
-    </Box>
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      flexDirection="column"
-      as="section"
-    >
-      <p>
-        Good:
-        <samp>{onStateGood}</samp>
-      </p>
-      <p>
-        Neutral:
-        <samp>{onStateNeutral}</samp>
-      </p>
-      <p>
-        Bad:
-        <samp>{onStateBad}</samp>
-      </p>
-    </Box>
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
+  <Box
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+    flexDirection="column"
+    as="section"
+  >
+    <p>
+      Good:
+      <samp>{good}</samp>
+    </p>
+    <p>
+      Neutral:
+      <samp>{neutral}</samp>
+    </p>
+    <p>
+      Bad:
+      <samp>{bad}</samp>
+    </p>
+    <p>
+      Total:
+      <samp>{total}</samp>
+    </p>
+    <p>
+      Positive feedbeck:
+      <samp>{positivePercentage}%</samp>
+    </p>
   </Box>
 );
 
